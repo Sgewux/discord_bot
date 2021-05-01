4ad62ce0f0f552e82dedb35601ed0514bfebb8e1
Sebastian Andrade <sebasandroide05@gmail.com>
2021-05-01 16:44:27 -0500

    Everything working fine, crypto commands working fine

--- bot.py
+++ bot.py
@@ -32,19 +32,24 @@ async def help(ctx):
     message_to_be_sent = """
     Hi 👋, my name is Diego Norrea 
     I'm a Discord bot 🤖 and i have the following commands:
-        General commands:
+
+        General commands 🌎:
             -weather: Given a place i will tell you the weather and temperature of that place.
             -HMD: To know how many days you have been on this server.
-        Space commands:
+
+        Space commands 🚀:
             -APOD: I will send the Astronomy Picture Of the Day.
             -MRP: I will send a random picture taken by the Curiosity mars rover.
-            -MRPD: I will send a photo taken by the Curiosity mars rover in date with the year month day format given by you(use blank spaces to split the values).
-        Crypto commands:
-            -cprice: Given a cryptocurrency name (bitcoin for example) and a normal currency (usd for example) i will send you the currentcryptocurrency price on that currency.
+            -MRPD: I will send a photo taken by the Curiosity mars rover in date with the year month day format given by you
+            (use blank spaces to split the values).
+
+        Crypto commands 💲:
+            -cprice: Given a cryptocurrency name (bitcoin for example) and a normal currency (usd for example) i will send you the current
+            price of that currency.
             -CTC: Given a normal currency name, cryptocurrency name and an amount i will Convert To Crypto that amount.
             -CFC: Given a cryptocurrency name, normal currency name and an amount i will Convert From Crypto that amount.
-
-            Use the full name to reffer to a crypto currency (type 'bitcoin' instead of 'btc') and use the abreviation to reffer to normal currencies (type 'usd' insted of 'United States Dollar')
+            Use the full name to reffer to a crypto currency (type 'bitcoin' instead of 'btc') and use the abreviation to reffer to normal
+            currencies (type 'usd' insted of 'United States Dollar')
 
         All commands should has '$'  as a prefix.
         The commands are not case sensitive(for me write apod is the same as write APOD)"""
@@ -92,7 +97,6 @@ async def time_belonging(ctx):
 @bot.command(pass_context=True, aliases=('MRPD', 'mrpd'))
 async def rover_photo_by_date(ctx, *args):
     date = '-'.join(args)
-    print(args)
     date_regex = re.compile('\d{4}-\d{1,2}-\d{1,2}')
     
     if date_regex.search(date):
@@ -112,21 +116,22 @@ async def get_price(ctx, crypto_name, currency_name):
 
 
 @bot.command(pass_context=True, aliases=('ctc', 'CTC'))
-async def conv_to_crypto(ctx, amount, currency_name, crypto_name):
+async def conv_to_crypto(ctx, amount: float, currency_name, crypto_name):
     currency_name = currency_name.lower()
     crypto_name = crypto_name.lower()
-    convertion = CryptoCommands.convert_to_crypto(currency_name, crypto_name, float(amount))
+    convertion = CryptoCommands().convert_to_crypto(currency_name, crypto_name, amount)
 
     await ctx.message.reply(content=convertion)
 
 
 @bot.command(pass_context=True, aliases=('cfc', 'CFC'))
-async def conv_from_crypto(ctx, amount, crypto_name, currency_name):
+async def conv_from_crypto(ctx, amount: float, crypto_name, currency_name):
     currency_name = currency_name.lower()
     crypto_name = crypto_name.lower()
-    convertion = CryptoCommands.convert_from_crypto(crypto_name, currency_name, float(amount))
+    convertion = CryptoCommands().convert_from_crypto(crypto_name, currency_name, amount)
 
     await ctx.message.reply(content=convertion)
+
 
 if __name__ == '__main__':
     bot.run(TOKEN)
